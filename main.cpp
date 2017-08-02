1db657bdaf042d4220ba776d6f2050ea02f01e8c
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-08-02 11:16:16 +0300

    slightly modified the interface

--- main.cpp
+++ main.cpp
@@ -5,20 +5,21 @@
 
 int main( int argc, char** argv) {
 
-  if( argc != 10 ) {
-    std::cerr << "Usage : ./main.out <N> <p_tri> <p_jump> <delta> <p_ld> <F> <q> <t_max> <seed>" << std::endl;
+  if( argc != 9 ) {
+    std::cerr << "Usage : ./main.out <N> <p_tri> <p_jump> <p_ld> <F> <q> <t_max> <seed>" << std::endl;
+    std::cerr << argc << std::endl;
     exit(1);
   }
 
   long net_size = std::atol( argv[1] );
   double p_tri  = std::atof( argv[2] );
   double p_jump = std::atof( argv[3] );
-  double delta  = std::atof( argv[4] );
-  double p_ld   = std::atof( argv[5] );
-  long F        = std::atol( argv[6] );
-  long q        = std::atol( argv[7] );
-  long t_max    = std::atol( argv[8] );
-  long seed     = std::atol( argv[9]);
+  double p_ld   = std::atof( argv[4] );
+  long F        = std::atol( argv[5] );
+  long q        = std::atol( argv[6] );
+  long t_max    = std::atol( argv[7] );
+  long seed     = std::atol( argv[8] );
+  double delta  = 1.0; //std::atof( argv[4] );
   double p_nd   = 0.0; // std::atof( argv[5] );
   double aging  = 1.0; // std::atof( argv[7] );
   double w_th   = 0.01; //std::atof( argv[8] );
@@ -29,7 +30,7 @@ int main( int argc, char** argv) {
             << "net_size:\t" << net_size << std::endl
             << "p_tri:\t" << p_tri << std::endl
             << "p_jump:\t" << p_jump << std::endl
-            << "delta:\t" << delta << std::endl
+            //<< "delta:\t" << delta << std::endl
             //<< "p_nd:\t" << p_nd << std::endl
             << "p_ld:\t" << p_ld << std::endl
             //<< "aging:\t" << aging << std::endl
