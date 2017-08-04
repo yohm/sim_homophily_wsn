2bdf5c0e941d249424cff826a643ae008c76e1ef
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-08-04 16:52:21 +0300

    move infomap submodule to network_analysis

--- run.sh
+++ run.sh
@@ -8,6 +8,4 @@ $SCRIPT_DIR/wsn.out $@
 $SCRIPT_DIR/network_analysis/run.sh net.edg
 $SCRIPT_DIR/network_analysis/plot/plot_all.sh .
 python $SCRIPT_DIR/plot_timeseries.py timeseries.dat
-$SCRIPT_DIR/infomap/Infomap net.edg . -i link-list -z -2 --overlapping --clu
-python $SCRIPT_DIR/parse_clu.py net.clu
 
